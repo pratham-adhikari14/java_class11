a19134f6cc9402a06ebbe0d2ba85b28f694063df
pratham-adhikari14 <prapumpum14@gmail.com>
2025-12-16 03:01:52 -0800

    update

---
+++ day_11/Guessnum.java
@@ -0,0 +1,31 @@
+class Guessnum {
+    int num;
+    int userInput;
+
+    Guessnum(int num) {
+       this.num = num;
+    }
+    public static void main(String [] args)
+ void Guessnum(){
+    
+    scanner input = new scanner(System.in)
+ }
+   while(true){
+    System.out.println("enter a number");
+    num2 = input.nextint();
+
+    if(userInput>num){
+        System.out.println("your number"+ userInput +"number is bigger");
+    }
+    else if (userInput<num){
+        System.out.println("your number" + userInput+ "number is smaller");
+    }
+    else{
+        System.out.println("your given number is equal")
+    }
+   }
+}
+
+
+
+
